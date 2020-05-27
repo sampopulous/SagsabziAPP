9b13f7cfb4e30a550d8938ae262428b111a1db40
sampopulous <65770070+sampopulous@users.noreply.github.com>
2020-05-28 01:10:48 +0530

    SagsabziAPP

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+var krms_config ={			
+	'ApiUrl':"http://sagsabzi.com/dev/singlemerchant/api",			
+	'DialogDefaultTitle':"Sagsabzi",			
+	'MerchantKeys':"e3cd02a02e475c9b1e9aa8ab252603b5",
+	'debug': false
+};
